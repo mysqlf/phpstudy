f0d31fbfd791e30824cd3143567659c4b6071b3e
langzi <1154505909@qq.com>
2017-04-11 10:43:09 +0800

    yaf mongodb lib

--- yaf/application/controllers/Index.php
+++ yaf/application/controllers/Index.php
@@ -14,7 +14,7 @@ class IndexController extends Controller_Abstract
         var_dump($request->getQuery());          //   输出: array()
         var_dump($request->getParam('id'));      //   输出：NULL
         var_dump($request->getParams());  */       //   输出：array()
-        /*$Test=new Test\Test();
+        $Test=new Test\Test();
         echo $Test->Index();
         echo "<br>";
         $Test=new Test();
@@ -27,7 +27,7 @@ class IndexController extends Controller_Abstract
         //导入一个函数库文件common.php，即可使用common.php中的函数
         Yaf\Loader::import(APP_PATH.'/application/helpers/common.php');
         var_dump(gethelper());
-        var_dump($this->getRequest()->getParam('id'));*/
+        var_dump($this->getRequest()->getParam('id'));
 
 
     }
@@ -46,17 +46,29 @@ class IndexController extends Controller_Abstract
      */
     public function testmongodbAction(){
         #创建连接
-       $manager = new MongoDB\Driver\Manager("mongodb://localhost:27017");
+      /* $manager = new MongoDB\Driver\Manager("mongodb://localhost:27017");
 
         // 插入数据
         $bulk = new MongoDB\Driver\BulkWrite;
 
-        $bulk->insert(['x' => 1, 'name'=>'菜鸟教程', 'url' => 'http://www.runoob.com']);
+        $bulk->insert();
         $bulk->insert(['x' => 2, 'name'=>'Google', 'url' => 'http://www.google.com']);
         $bulk->insert(['x' => 3, 'name'=>'taobao', 'url' => 'http://www.taobao.com']);
         $bulk->insert(['x' => 4, 'name'=>null, 'url' => 'http://www.taobao.com']);
-        $manager->executeBulkWrite('test.sites', $bulk);
-
+        $manager->executeBulkWrite('test.sites', $bulk);*/
+        $mongodb=new Database\Mongo();
+        $list=['x' => 1, 'name'=>'langzi', 'url' => 'http://wp.greedy.bid'];
+        $table='test.lz';
+        $mongodb->add($table,$list);
+        $filter=['x' => ['$gt' => 0]];
+        $options=[ 
+            'projection' => ['_id' => 0],
+            'sort' => ['x' => 1],
+            ];
+        $document=$mongodb->search($table,$filter,$options);
+        foreach ($document as  $value) {
+            var_dump($value);
+        }
         /*$filter = ['x' => ['$gt' => 0]];
         $options = [
             'projection' => ['_id' => 0],
@@ -70,7 +82,7 @@ class IndexController extends Controller_Abstract
             var_dump($document);
         }*/
 
-        $filter = ['name' => ['$type' =>10]];
+       /* $filter = ['name' => ['$type' =>10]];
         $options = [
             'projection' => ['_id' => 1],#  0 不返回_id  1返回_id
             'sort' => ['x' => 1],
@@ -105,19 +117,34 @@ class IndexController extends Controller_Abstract
         $bulk->delete(['x' => 2], ['limit' => 0]);   // limit 为 0 时，删除所有匹配数据
         $bulk->delete(['x' => 3], ['limit' => 0]);   // limit 为 0 时，删除所有匹配数据
         $bulk->delete(['x' => 4], ['limit' => 0]);   // limit 为 0 时，删除所有匹配数据
-        $result = $manager->executeBulkWrite('test.sites', $bulk, $writeConcern);
+        $result = $manager->executeBulkWrite('test.sites', $bulk, $writeConcern);*/
 
     }
     public function gogogoAction(){
         $manager=new MongoDB\Driver\Manager("mongodb://localhost:27017");
-        $bulk=new MongoDB\Driver\BulkWrite;
-        $document=['_id'=>new MongoDB\BSON\ObjectID,'name'=>'LZYX'];
-        $_id=$bulk->insert($document);
-        $write=new MongoDB\Driver\WriteConcern(MongoDB\Driver\WriteConcern::MAJORITY,1000);
-        $result=$manager->executeBulkWrite('test.sites',$bulk,$write);
-        var_dump($_id);
-        var_dump($result);
-        $filter=['name'=>['$text'=>'L']];
+        // 插入数据
+        $bulk = new MongoDB\Driver\BulkWrite;
+
+        $bulk->insert(['x' => 1, 'name'=>'LZYX', 'url' => 'http://www.runoob.com']);
+        $bulk->insert(['x' => 2, 'name'=>'LZYX', 'url' => 'http://www.google.com']);
+        $bulk->insert(['x' => 3, 'name'=>'taobao', 'url' => 'http://www.taobao.com']);
+        $bulk->insert(['x' => 4, 'name'=>null, 'url' => 'http://www.taobao.com']);
+        $manager->executeBulkWrite('test.sites', $bulk);
+
+        $filter = ['name' => ['$type' =>10]];
+        $options = [
+            'projection' => ['_id' => 0],#  0 不返回_id  1返回_id
+            'sort' => ['x' => 1],
+        ];
+        // 查询数据
+        $query = new MongoDB\Driver\Query($filter, $options);
+        $cursor = $manager->executeQuery('test.sites', $query);
+
+        foreach ($cursor as $document) {
+            var_dump($document);
+        }
+      
+        $filter=['name'=>['$'=>'LZYX']];
         $options = [
             'projection' => ['_id' => 0],
             'sort' => ['x' => -1],
---
+++ yaf/library/Database/Mongo.php
@@ -0,0 +1,73 @@
+<?php
+namespace Database;
+class Mongo{
+    public function __construct(){
+        $this->manager = new \MongoDB\Driver\Manager("mongodb://localhost:27017");
+    }
+    /**
+     * [add 插入]
+     * @author Greedywolf 1154505909@qq.com
+     * @DateTime 2017-04-11
+     * @param    [arr]     $list  [数据]
+     * @param    [str]     $table [表]
+     * @return   [type]            [description]
+     */
+    public function add($table,$list){
+        $bulk = new \MongoDB\Driver\BulkWrite;
+        $writeConcern = new \MongoDB\Driver\WriteConcern(\MongoDB\Driver\WriteConcern::MAJORITY, 1000);
+        $bulk->insert($list);
+        $this->manager->executeBulkWrite($table, $bulk,$writeConcern);
+        return '';
+    }
+    /**
+     * [search 查询数据]
+     * @author Greedywolf 1154505909@qq.com
+     * @DateTime 2017-04-11
+     * @param    [str]     $table   [表]
+     * @param    [arr]     $filter  [条件]
+     * @param    [arr]     $options [选项]
+     * @return   [arr]              [查询结果]
+     */
+    public function search($table,$filter,$options){
+        $query = new \MongoDB\Driver\Query($filter, $options);
+        $cursor = $this->manager->executeQuery($table, $query);
+        return $cursor;
+    }
+    /**
+     * [update 修改]
+     * @author Greedywolf 1154505909@qq.com
+     * @DateTime 2017-04-11
+     * @param    [str]     $table [表]
+     * @param    [arr]     $where [条件]
+     * @param    [arr]     $set   [修改值]
+     * @param    [arr]     $multi [限制选项]
+     * @return   [type]            [description]
+     */
+    public function update($table,$where,$set,$multi=['multi' => false, 'upsert' => false]){
+        if (empty($where)) {
+            return 'where is empty';
+        }
+        $bulk = new \MongoDB\Driver\BulkWrite;
+        $bulk->update($where, ['$set' => $set], $multi);
+        $writeConcern = new \MongoDB\Driver\WriteConcern(\MongoDB\Driver\WriteConcern::MAJORITY, 1000);
+        $result = $this->manager->executeBulkWrite($table, $bulk,$writeConcern);
+        return '';
+    }
+    /**
+     * [delete 删除数据]
+     * @author Greedywolf 1154505909@qq.com
+     * @DateTime 2017-04-11
+     * @param    [str]     $table [表]
+     * @param    [arr]     $where [条件]
+     * @param    [arr]     $limit [限制]
+     * @return   [type]            [description]
+     */
+    public function delete($table,$where,$limit=['limit' => 1]){
+        if (empty($where)) {
+            return 'where is empty';
+        }
+        $bulk = new \MongoDB\Driver\BulkWrite;
+        $bulk->delete($where,$limit);   // limit 为 1 时，删除第一条匹配数据
+        $result = $this->manager->executeBulkWrite($table, $bulk, $writeConcern);
+    }
+}
